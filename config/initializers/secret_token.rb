231351e9ed3227b373193789650b65ced7621554
Blurredbits <mmorris00@mac.com>
2014-03-19 20:59:08 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodtruckrodeo::Application.config.secret_key_base = '8c0daae581a07b560bfc38a05f702770126e96a11823bf3460b6bb9aa6e4e4dcfd47d03983acafe6d54010ed2e6838cd078b1eb3d7cf6cb30c47516abde51924'
