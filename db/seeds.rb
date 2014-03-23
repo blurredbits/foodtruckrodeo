f1bce8334974c86dd4c9383a36d29a884461419e
Raman Sinha <velobuff@gmail.com>
2014-03-22 20:44:03 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+FoodTruck.create(name: "Mike's MexicTruck", address: 'Broomfield, CO', url:'mikestrucks.com')
+FoodTruck.create(name: "Mark's Food Truck", address: 'Fort Collins, CO', url:'mikestrucks.com')
+FoodTruck.create(name: "Beth's Mountain Waffles", address: 'Park City, UT', url:'waffleybeth.com')
+FoodTruck.create(name: "Raman's Food Truck", address: 'Lafayette, CO', url:'velobuffsfood.com')
+FoodTruck.create(name: "Carl's Bee Beer Truck", address: 'Sandy, UT', url:'carlsbeebeer.com')
