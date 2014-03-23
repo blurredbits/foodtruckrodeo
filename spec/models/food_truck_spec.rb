1fbfaeaf6f3f6425893065dbed7c6bd7a457734e
Blurredbits <mmorris00@mac.com>
2014-03-22 19:14:39 -0600

    Updated rspec to use built in nil matcher

--- spec/models/food_truck_spec.rb
+++ spec/models/food_truck_spec.rb
@@ -5,8 +5,8 @@ describe FoodTruck do
   let(:sample_truck) { FactoryGirl.create(:food_truck) }
 
   it 'geocodes address' do
-    expect(sample_truck.longitude).to_not be(:nil)
-    expect(sample_truck.latitude).to_not be(:nil)
+    expect(sample_truck.longitude).to_not be_nil
+    expect(sample_truck.latitude).to_not be_nil
   end
 
 end
